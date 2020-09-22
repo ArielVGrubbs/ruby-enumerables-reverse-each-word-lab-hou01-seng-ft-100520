3044096faa492a3b0256c42f1107c67321621c23
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-22 23:32:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   string_array = string.split
   string_array.collect { |string|
-  a = string
-  a.reverse
+  #a = string
+  string.reverse!
   }
   string = string_array.join (" ")
   string
