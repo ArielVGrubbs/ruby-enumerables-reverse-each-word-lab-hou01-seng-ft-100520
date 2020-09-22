f1213b885c85f4d2e335839f2ef9a4d0da657464
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-22 23:32:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(string)
   string_array = string.split
   string_array.collect { |string|
-  p string
-  string.reverse
-  p string
+  a = string
+  a.reverse
   }
   string = string_array.join (" ")
   string
